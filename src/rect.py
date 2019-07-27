3599458d6f0e123cf333b9cc26bf394ea0f73ee7
kwonbosung02 <rnjsqhtjd0526@naver.com>
2019-07-28 01:30:28 +0900

    commit

---
+++ src/rect.py
@@ -0,0 +1,12 @@
+
+import cv2
+import numpy as np
+import copy
+import math
+
+cap_region_x_begin=0.5
+cap_region_y_end=0.8
+threshold = 60
+blurValue = 41
+background_Subtract_Threshold = 50
+learningRate = 0
