889b21108c6c3c62b8d854fa4851b9487cf19865
Kwonbosung02 <rnjsqhtjd0526@naver.com>
2019-11-01 09:40:06 +0900

    commit

--- src/serial/serial.ino
+++ src/serial/serial.ino
@@ -2,21 +2,13 @@ void setup() {
   Serial.begin(9600);
   pinMode(13,OUTPUT);
 }
- char get = ' ';
+ int get = 0;
 void loop() {
    
     
-    get = Serial.read();      
-    
-    if(get == '3' || get == '4' || get == '5'){
-      digitalWrite(13,HIGH);
-      
-      Serial.println(get);
-    
-    }
-    else if(get == '0' || get == '1' || get == '2'){
-      digitalWrite(13,LOW);
-    }
+    get = Serial.read();
+    check_0(get);
+
 
 
  
